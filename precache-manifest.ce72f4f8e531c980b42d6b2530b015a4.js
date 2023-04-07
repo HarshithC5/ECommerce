955f1c452ac4b480fcda21aa866696ba6dbaad87
Harshith C Shetty <harshithcshetty393@gmail.com>
2023-04-07 22:00:50 +0530

    Updates

---
+++ precache-manifest.ce72f4f8e531c980b42d6b2530b015a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4d32450e662cff0eb725",
+    "url": "/HarshithC5/ECommerce/static/css/main.3041fd06.chunk.css"
+  },
+  {
+    "revision": "4d32450e662cff0eb725",
+    "url": "/HarshithC5/ECommerce/static/js/main.4d32450e.chunk.js"
+  },
+  {
+    "revision": "4951e6e2493b9773d671",
+    "url": "/HarshithC5/ECommerce/static/css/1.ef168a58.chunk.css"
+  },
+  {
+    "revision": "4951e6e2493b9773d671",
+    "url": "/HarshithC5/ECommerce/static/js/1.4951e6e2.chunk.js"
+  },
+  {
+    "revision": "f40afdb242dc05fa2c69",
+    "url": "/HarshithC5/ECommerce/static/js/runtime~main.f40afdb2.js"
+  },
+  {
+    "revision": "4f5abcfec4d1d1535107eb981446f617",
+    "url": "/HarshithC5/ECommerce/index.html"
+  }
+];
